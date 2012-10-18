5dd8a4af64abc1cba9fffe47793d250fc33510c9
Thyago Quintas <thyagoquintas@hotmail.com>
2012-10-18 10:15:43 -0700

    v1.0.2
    Change the element to get the mousemove event

--- jquery.resizable.js
+++ jquery.resizable.js
@@ -1,6 +1,7 @@
 /*!
- * jQuery Resizable plugin v1.0.1
+ * jQuery Resizable plugin v1.0.2
  * 
+ * v1.0.2: Change the element to get the mousemove
  * v1.0.1: Fixed math mistake.
  *
  * Copyright 2012, Thyago Quintas (dev@thyagoquintas.com.br)
@@ -13,7 +14,7 @@
 (function($) {
     $.fn.resizable = function(contents) {
         contents = $.extend({
-        	cursor : "nw-resize",
+        	cursor : 'nw-resize',
         	onResizeStart : function() { },
         	onResizing : function() { },
         	onResizeEnd : function() { }
@@ -24,13 +25,13 @@
 	    var onResizeEnd = contents.onResizeEnd;
         var $element = this;
         
-        return $element.css('cursor', contents.cursor).on("mousedown", function(e) {
+        return $element.css('cursor', contents.cursor).on('mousedown', function(e) {
             onResizeStart($($(this).parents()[0]));
             var $element = $(this);
            
             var $element_parent = $($element.parents()[0]).addClass('resizable');
-            
-                $('*').on("mousemove", function(e) {
+            	
+                $(window).on('mousemove', function(e) {
 	                onResizing(e);
 	                var pos_y = $element_parent.offset().top,
 	                	pos_x = $element_parent.offset().left;
@@ -39,9 +40,9 @@
 	                    'width' :  e.pageX - pos_x,
 	                    'height' : e.pageY - pos_y
 	                });
-	            }).on("mouseup", function(){
-	              	$(this).unbind('mousemove');
-	            	$(this).removeClass('resizable');
+	            }).on('mouseup', function(){
+	              	$(window).unbind('mousemove');
+	            	$element_parent.removeClass('resizable');
 	            });
             e.preventDefault();
         }).on("mouseup", function() {
